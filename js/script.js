664a74958734483607d06d854903fb91994c6751
Dan Blundell <danblundell@me.com>
2015-05-12 14:31:22 +0100

    add basic mobile navigation for the global header

--- js/script.js
+++ js/script.js
@@ -5,5 +5,12 @@ $(function() {
       $(this).next(".code").slideToggle("fast");
     //  $(this).find("i").toggleClass("fa-arrow-circle-down").toggleClass("fa-arrow-circle-right");
   });
+
+  // sharepoint mobile navigation
+  $('.js-menu-btn').delegate('a','click',function(e){
+    e.preventDefault();
+    // toggle the menu
+    $('.js-menu').toggleClass('is-open');
+  });
   
 });
